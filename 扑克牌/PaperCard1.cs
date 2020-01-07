7832546c75505401beaffb0a99637b2a1e1a9310
17774813651 <58196401+17774813651@users.noreply.github.com>
2020-01-07 11:12:38 +0800

    提交

---
+++ 扑克牌/PaperCard1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCard1
+{
+
+
+        public class PaperCard
+        {
+            public string Type { get; set; }
+            public string Number { get; set; }
+            public PaperCard(string strType, string strNumber)
+            {
+                this.Type = strType;
+                this.Number = strNumber;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return string.Format("{0}{1}", this.Type, this.Number);
+                }
+
+            private set { }
+        }
+        }
+
+}
---
+++ 扑克牌/User1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CDS000.Common;
+using PaperCard1;
+
+namespace User1
+{
+    public class Person
+    {
+        public class User
+        {
+            private List<PaperCard>
+            listCard = new List<PaperCard>
+                ();
+
+            public string Name { get; set; }
+
+            public User(string strName)
+            {
+                this.Name = strName;
+            }
+            public void Add(PaperCard p)
+            {
+                listCard.Add(p);
+            }
+
+            public void Reset()
+            {
+                listCard.Clear();
+            }
+
+            public void Introduce()
+            {
+                string strCards = "";
+
+                for (int i = 0; i < listCard.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        strCards += listCard[i].Name;
+                    }
+                    else
+                    {
+                        strCards += ", " + listCard[i].Name;
+                    }
+                }
+                Console.WriteLine("\r\n--------------------------------------------");
+                Console.Write("我是{0}，", Name);
+                Console.Write("我手中的扑克牌有{0}张:\r\n{1}\r\n", listCard.Count, strCards);
+            }
+        }
+
+    }
+}
